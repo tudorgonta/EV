10e9637e4ee0a40fe646511b3604edfc2d0b011b
tudorgonta <55688652+tudorgonta@users.noreply.github.com>
2022-08-29 23:06:48 +0100

    Navbar Fix

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -6,6 +6,7 @@ import {useRouter} from 'next/router'
 
 function MainNavigation() {
   const router = useRouter();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   const { data: session, status } = useSession();
   const loading = status === 'loading';
@@ -17,16 +18,13 @@ function MainNavigation() {
   const [stickyClass, setStickyClass] = useState('');
   const [linkClass, setLinkClass] = useState('');
 
-  useEffect(() => {
-    window.addEventListener('scroll', stickNavbar);
-    window.addEventListener('load', stickNavbar)
-  }, []);
+
 
   const stickNavbar = () => {
     if (window !== undefined) {
       let windowHeight = window.scrollY;
-      windowHeight > 100 ? (
-        setStickyClass('bg-[#FFFFFF] bg-opacity-100 text-black shadow-md z-10'),
+      (windowHeight > 100) ? (
+        setStickyClass('bg-[#FFFFFF] bg-opacity-100 text-black shadow-md z-50'),
         setLinkClass('border-black')
         ) : (setStickyClass('bg-black bg-opacity-20 text-white'),
            setLinkClass('')
@@ -34,17 +32,46 @@ function MainNavigation() {
     }
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', stickNavbar);
+    window.addEventListener('load', stickNavbar);
+    }, []);
+
   return (
    
-    <div className={router.pathname == "/" ? `text-white fixed w-full transition-all duration-300 ${stickyClass}` : `fixed w-full bg-white/100 text-black top-0 shadow-md z-10`}>
+    <div className={router.pathname == "/" ? `text-white fixed w-full transition-all duration-300 ${stickyClass}` : `fixed w-full bg-white/100 text-black top-0 shadow-md z-50`}>
     <header className="flex flex-row justify-between w-10/12 m-auto py-4">
       <Link href='/'>
         <a className=''>
           <div className="font-semibold">EV Charging</div>
         </a>
       </Link>
+
+      <section className="MOBILE-MENU flex lg:hidden">
+
+        <div className="HAMBURGER-ICON space-y-2" onClick={() => setIsNavOpen((prev) => !prev)}>
+          <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+        </div>
+
+        <div className={isNavOpen ? `showMenuNav bg-opacity-100` : "hideMenuNav"}>
+          {/* X sign */}
+            <div className={`fixed top-0 right-0 px-8 py-8`} onClick={() => setIsNavOpen(false)} >
+              <svg className="h-8 w-8 text-gray-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            {/* MENU */}
+            <ul className="flex flex-col items-center justify-between min-h-[250px] text-black z-50">
+              <li>Item</li>
+            </ul>
+          </div>
+
+      </section>
       <nav>
-        <ul className='flex flex-row'>
+        <ul className='hidden lg:flex lg:flex-row'>
             <li className='font-medium'>
               <Link href='/enquiry'><a className={`hover:border-b transition-all duration-100 ${linkClass}`}>Make an Enquiry</a></Link>
             </li>
